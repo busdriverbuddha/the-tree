79b500ca38bee03b3890cb69747fd6133cc99242
Guilherme GAma <guilhermegama@gmail.com>
2025-10-05 09:29:51 -0300

    staqshing

--- dumpcode.py
+++ dumpcode.py
@@ -12,6 +12,12 @@ parser.add_argument(
     default="backend",
 )
 
+COMMENT_PREFIX = {
+    '.py': "#",
+    '.js': "//",
+    '.jsx': "//",
+}
+
 GLOB_PATTERNS = {
     'backend': ["*.py"],
     'frontend': [],
@@ -26,6 +32,24 @@ BASEDIRS = {
     'backend': Path("./the_tree"),
     'frontend': Path("./the-tree-frontend"),
 }
+
+
+def tag_file(rel_path: Path, base_dir: Path):
+    prefix = COMMENT_PREFIX.get(rel_path.suffix, None)
+    abspath = base_dir / rel_path
+    tag = f"{prefix} {rel_path}"
+    print(tag)
+
+    if prefix is None:
+        return
+
+    lines = abspath.read_text().splitlines()
+    while lines and (lines[0].strip() == "" or lines[0].strip().startswith(prefix)):
+        lines.pop(0)
+
+    lines = [tag, ""] + lines + [""]
+    text = "\n".join(lines)
+    abspath.write_text(text)
 
 
 def get_file_list(module: str) -> list[Path]:
@@ -45,9 +69,10 @@ def get_file_list(module: str) -> list[Path]:
 
 
 def main(module: str):
+    basedir = BASEDIRS[module]
     relpaths = get_file_list(module)
     for rp in relpaths:
-        print(rp)
+        tag_file(rp, basedir)
 
 
 if __name__ == "__main__":
